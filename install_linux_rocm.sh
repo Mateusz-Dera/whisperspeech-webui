8a28b9586446638369a86187caa5c76ee66b5201
Mateusz-Dera <OtahoPL@gmail.com>
2024-02-05 20:56:18 +0100

    rocm

--- install_linux_rocm.sh
+++ install_linux_rocm.sh
@@ -4,7 +4,4 @@ if ! command -v python3.11 &> /dev/null; then
 fi
 python3.11 -m venv .venv
 source .venv/bin/activate
-git clone https://github.com/collabora/WhisperSpeech.git
-cd WhisperSpeech
-git checkout 80b268b74900b2f7ca7a36a3c789607a3f4cd912
-pip install -e .
+pip install -r requirements_rocm.txt --extra-index-url https://download.pytorch.org/whl/rocm5.7
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 source .venv/bin/activate
-python3 webui.py
+python3 webui.py --port 8004
